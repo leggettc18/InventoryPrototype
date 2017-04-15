3eaae6ab52402fa93dc3b7ce73278ff1be1852f2
Christopher Leggett <xerranort@gmail.com>
2017-04-15 14:15:08 +0000

    Adds more image versions for different size images

--- inventorymanager/app/models/item.rb
+++ inventorymanager/app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+    
+    mount_uploaders :images, ImageUploader
+    serialize :images, JSON
+    
     enum quality: ["New", "Like New", "Very Good", "Acceptable", "Poor"]
     enum category: ["Furniture", "Electronics", "Appliances", "Clothes", "Other"]
 end
